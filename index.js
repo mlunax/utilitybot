9e7be6ec520bc70714a65e536afe5fd951e4b6ad
lunax <mlunaelumen@gmail.com>
2018-09-15 16:56:37 +0200

    Add Config File

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
+const config = require('./config.json');
+
+if(config.main.token == ""){
+    console.log("Set the token in config.json");
+    return;
+}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -11,4 +18,4 @@ client.on('message', msg => {
   }
 });
 
-client.login('token');
+client.login(config.main.token);
